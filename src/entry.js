23447711cbcf166b5d7fb97f714e8cd3a5c1af6c
Charlyhm79 <charly_hm_@hotmail.com>
2018-02-20 00:36:58 +0100

    implementamos babel

--- src/entry.js
+++ src/entry.js
@@ -1,2 +1,11 @@
 
-console.log('Hello world');
+
+import css from './style.scss';
+
+import moment from 'moment';
+
+document.querySelector("body").addEventListener("click", (event) =>{
+    console.log("Hello");
+});
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,60 @@
 
 const path = require('path');
-
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports= {
 
     //entry point : archivo que lee webpack para construir el grafo de dependendcias
 
-    entry: './src/entry.js',
+    entry: path.join(__dirname , "src", "entry.js"),
 
     //output: carpeta en la que quiero que webpack me deje elcodigo generado
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname ,'dist')
+    },
+
+    // module loaders
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },{
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: path.join(__dirname, 'node_modules')
+            }
+        ]
+    },
+
+
+    //plugins que estamos utilizando
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname , "src" , "index.html"), 
+
+            minify: {
+                collapseWhitespace: true
+
+            }
+               
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+
+    ],
+    // dev server configuration
+
+    devServer: {
+        open: true, // abre el navegador por defecto
+        port: 3000, // puerto del servidor web
+        overlay: true, // muestra los errores en pantalla
+        hot: true,
+        contentBase: path.join(__dirname, 'src'),
+        watchContentBase: true
     }
 
 };
